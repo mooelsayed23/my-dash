0f430dffb0bcf8abc76a7081e7c78c7365922e76
ahmed <ahmed.mooelsayed@gmail.com>
2024-02-22 23:32:02 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "readymadeui.com",
-      },
-      {
-        protocol: "https",
-        hostname: "m.media-amazon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
-  },
+  
 };
 
 export default nextConfig;

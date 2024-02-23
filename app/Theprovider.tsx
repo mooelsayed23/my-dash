1893f6b07a817e36f51b6c2bcfba79ddea605403
ahmed <ahmed.mooelsayed@gmail.com>
2024-02-23 03:57:00 +0200

    table

--- app/Theprovider.tsx
+++ app/Theprovider.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { store } from "@/redux/store";
+import { NextUIProvider } from "@nextui-org/react";
 import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 interface Props {
   children: ReactNode;
 }
 const Theprovider = ({ children }: Props) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </Provider>
+  );
 };
 
 export default Theprovider;

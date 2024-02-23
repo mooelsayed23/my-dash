1c8547041002ae61c79304d2b06dcf6045f4e828
ahmed <ahmed.mooelsayed@gmail.com>
2024-02-23 03:31:48 +0200

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -20,5 +19,5 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-export default config;
+}
+

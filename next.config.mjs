8bc273c10d6f34d6e14e964e9e5ef4cc9087eade
ahmed <ahmed.mooelsayed@gmail.com>
2024-03-23 13:06:19 +0200

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.unsplash.com",
-          port: '',
-          pathname: '/*',          
-        },
-        {
-          protocol: "https",
-          hostname: "readymadeui.com",
-          port: '',
-        },
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: "https",
+    //       hostname: "images.unsplash.com",
+    //       port: '',
+    //       pathname: '/*',          
+    //     },
+    //     {
+    //       protocol: "https",
+    //       hostname: "readymadeui.com",
+    //       port: '',
+    //     },
+    //   ],
+    // },
+    images:{
+      domains : [
+        "images.unsplash.com","readymadeui.com"
       ],
-    },
+    }
   };
   
   export default nextConfig;
